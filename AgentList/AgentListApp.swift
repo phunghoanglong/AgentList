f02a4aa51ad2b6d0e2e490733bc2b71900bf5405
phunghoanglong <s3965673@rmit.edu.vn>
2024-08-06 04:25:41 +0700

    Initial Commit

---
+++ AgentList/AgentListApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgentListApp.swift
+//  AgentList
+//
+//  Created by Long Phung on 6/8/24.
+//
+
+import SwiftUI
+
+@main
+struct AgentListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
